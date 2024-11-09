46693cf7aaa1284e172ccd194a99995c66d98068
filipe-muniz <filipe.munizsh@gmail.com>
2024-11-09 11:31:41 -0300

    feat: ✨ DTOS for Groq request/response

--- src/main/java/com/filipe/feedback_analyzer/dto/GroqRequest.java
+++ src/main/java/com/filipe/feedback_analyzer/dto/GroqRequest.java
@@ -1,4 +1,28 @@
 package com.filipe.feedback_analyzer.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+@Getter
+@Setter
 public class GroqRequest {
+    private String model;
+    private List<Message> messages;
+    public GroqRequest(String model, String content) {
+        this.model = model;
+        this.messages = List.of(new Message("user", content));
+        };
+    @Getter
+    @Setter
+    public static class Message{
+        private String role;
+        private String content;
+        public Message(String role, String content) {
+            this.role = role;
+            this.content = content;
+        }
+    }
 }
--- src/main/java/com/filipe/feedback_analyzer/dto/GroqResponse.java
+++ src/main/java/com/filipe/feedback_analyzer/dto/GroqResponse.java
@@ -1,4 +1,21 @@
 package com.filipe.feedback_analyzer.dto;
-
+import java.util.List;
 public class GroqResponse {
+    private List<Choice>choices;
+    public List<Choice> getChoices(){
+        return choices;
+
+    }
+    public static class Choice {
+        private Message message;
+        public Message getMessage(){
+            return message;
+        }
+    }
+    public static class Message{
+        private String content;
+        public String getContent(){
+            return content;
+        }
+    }
 }
