3a038c6c9911e648e26a85df55e9060d8311b0d7
filipe <77029323+filipe-muniz@users.noreply.github.com>
2024-11-09 10:23:27 -0300

    feat: :wrench: adding initial dependencies and config GROQ
    initial spring config and auth key(env set) and url groq

---
+++ src/test/java/com/filipe/feedback_analyzer/FeedbackAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filipe.feedback_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
