96996c7278333a42932fc4c1b6bd42c91a3b4cc7
filipe-muniz <filipe.munizsh@gmail.com>
2024-11-10 15:40:34 -0300

    feat: ✨adding custom exception for failed classification

---
+++ src/main/java/com/filipe/feedback_analyzer/exceptions/FeedbackClassificationException.java
@@ -0,0 +1,7 @@
+package com.filipe.feedback_analyzer.exceptions;
+
+public class FeedbackClassificationException extends RuntimeException{
+    public FeedbackClassificationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
