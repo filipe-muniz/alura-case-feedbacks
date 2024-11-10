cba79e97d62394243d9cb6c1af3f496248a66723
filipe-muniz <filipe.munizsh@gmail.com>
2024-11-10 16:29:18 -0300

    refactor: ♻️spam prompt to broad

--- src/main/java/com/filipe/feedback_analyzer/classifier/spam/GroqSpamClassifier.java
+++ src/main/java/com/filipe/feedback_analyzer/classifier/spam/GroqSpamClassifier.java
@@ -19,13 +19,9 @@ public class GroqSpamClassifier implements Classifier {
 
     @Override
     public String classify(String text) {
-        String prompt = String.format(
-                "Contexto: A AluMind é uma startup que oferece um aplicativo focado em bem-estar e saúde mental, proporcionando aos usuários acesso a meditações guiadas, sessões de terapia e conteúdos educativos sobre saúde mental. Com o crescimento acelerado da base de usuários, surgiram desafios para analisar os feedbacks recebidos de várias plataformas (como canais de atendimento ao cliente, comunidades no Discord e redes sociais). A tarefa é auxiliar na classificação desses feedbacks como SPAM ou não.\n\n" +
-                        "Agora, verifique se o seguinte feedback é um SPAM.\n" +
-                        "Feedback: \"%s\"\n\n" +
-                        "Por favor, forneça apenas \"SIM\" se for SPAM ou \"NAO\" se não for:",
-                text
-        );
+        String prompt = String.format("Verifique se o seguinte feedback é um SPAM. " +
+                        "\n\nFeedback: \"%s\"\n\nPor favor, forneça apenas \"SIM\" se for SPAM ou \"NÃO\" se não for:",
+                text);
         Optional<String> response = groqClient.requestPrompt("llama3-8b-8192", prompt);
         if (response.isPresent()) {
             String classification = response.get().trim().toUpperCase();
